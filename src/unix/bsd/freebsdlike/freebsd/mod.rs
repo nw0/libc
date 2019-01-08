5936262063704db85d24e53d28f1a4a2925cf868
Khilan Gudka <Khilan.Gudka@cl.cam.ac.uk>
2019-01-08 11:24:04 +0000

    Add types for CHERI on FreeBSD

--- src/unix/bsd/freebsdlike/freebsd/mod.rs
+++ src/unix/bsd/freebsdlike/freebsd/mod.rs
@@ -1162,6 +1162,9 @@ cfg_if! {
     } else if #[cfg(target_arch = "aarch64")] {
         mod aarch64;
         pub use self::aarch64::*;
+    } else if #[cfg(target_arch = "cheri")] {
+        mod cheri;
+        pub use self::cheri::*;
     } else {
         // Unknown target_arch
     }
